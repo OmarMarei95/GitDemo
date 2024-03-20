99a5a8b7913793949c90252b334356f3f1c8565d
Marei <omar.yahya.4556@gmail.com>
2024-03-21 01:30:07 +0200

    Creating develop branch

--- src/DynamicJson.java
+++ src/DynamicJson.java
@@ -33,4 +33,5 @@ System.out.println("Git Trial");
 System.out.println("Testing Git integration between two projects");
 System.out.println("Testing GitDemo2");
 System.out.println("wa7eeed taamaaaaaam??");
+System.out.println("creating Develop branch");
 }
